7e2ab04abf02e0e3514ba6ac7cd8b2501f0f8380
Patrick Tak <lodzic@gmail.com>
2016-11-21 22:45:01 +0100

    Mouse Manager
    Added mouse manager to select and deselect units

---
+++ RTS Project/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MouseManager : MonoBehaviour {
+
+    private List<Interactive> _selections = new List<Interactive>();
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        var es = UnityEngine.EventSystems.EventSystem.current;
+        if (es != null && es.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (_selections.Count > 0)
+        {
+            if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.RightShift))
+            {
+                foreach (var sel in _selections)
+                {
+                    if (sel != null) sel.Deselect();
+                }
+                _selections.Clear();
+            }
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (!Physics.Raycast(ray, out hit))
+        {
+            return;
+        }
+
+        var interact = hit.transform.GetComponent<Interactive>();
+        if (interact == null)
+        {
+            return;
+        }
+        else
+        {
+            _selections.Add(interact);
+            interact.Select();
+        }
+	}
+}
